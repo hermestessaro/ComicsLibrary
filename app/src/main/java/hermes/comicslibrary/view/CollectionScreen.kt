bd426d5f5b88ff32d1c3df0d74d4ccc52a9f5d44
Hermes Tessaro <haffonso@nelogica.com.br>
2023-07-16 00:14:42 -0300

    updating UI with notes

--- app/src/main/java/hermes/comicslibrary/view/CollectionScreen.kt
+++ app/src/main/java/hermes/comicslibrary/view/CollectionScreen.kt
@@ -1,5 +1,6 @@
 package hermes.comicslibrary.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,14 +10,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
@@ -24,14 +30,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import hermes.comicslibrary.model.Note
+import hermes.comicslibrary.model.db.DbNote
+import hermes.comicslibrary.ui.theme.GrayBackground
+import hermes.comicslibrary.ui.theme.GrayTransparentBackground
 import hermes.comicslibrary.utils.CharacterImage
 import hermes.comicslibrary.viewmodel.CollectionDbViewModel
 
@@ -41,6 +54,7 @@ fun CollectionScreen(collectionViewModel: CollectionDbViewModel, navController: 
     val charactersInCollection = collectionViewModel.collection.collectAsState()
     val expandedElement = remember { mutableStateOf(-1) }
     //notes can be added to elements, but will only be shown if the element is expanded. This variable is a flag
+    val notes = collectionViewModel.notes.collectAsState()
 
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(charactersInCollection.value) { character ->
@@ -95,13 +109,27 @@ fun CollectionScreen(collectionViewModel: CollectionDbViewModel, navController: 
                             }
                         )
 
-                        if (character.id == expandedElement.value){
+                        if (character.id == expandedElement.value) {
                             Icon(Icons.Outlined.KeyboardArrowUp, contentDescription = null)
-                        }
-                        else {
+                        } else {
                             Icon(Icons.Outlined.KeyboardArrowDown, contentDescription = null)
                         }
                     }
+                }
+            }
+
+            if (character.id == expandedElement.value) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(GrayTransparentBackground)
+                ) {
+                    val filteredNotes = notes.value.filter { note ->
+                        note.characterId == character.id
+                    }
+                    NotesList(filteredNotes, collectionViewModel)
+                    CreateNoteForm(character.id, collectionViewModel)
                 }
             }
 
@@ -114,4 +142,80 @@ fun CollectionScreen(collectionViewModel: CollectionDbViewModel, navController: 
         }
     }
 
+}
+
+@Composable
+fun NotesList(notes: List<DbNote>, collectionViewModel: CollectionDbViewModel) {
+    for (note in notes) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .background(GrayBackground)
+                .padding(4.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = note.title, fontWeight = FontWeight.Bold)
+                Text(text = note.text)
+            }
+            Icon(
+                Icons.Outlined.Delete,
+                contentDescription = null,
+                modifier = Modifier.clickable {
+                    collectionViewModel.deleteNote(note)
+                })
+        }
+    }
+}
+
+@Composable
+fun CreateNoteForm(characterId: Int, collectionViewModel: CollectionDbViewModel) {
+    val addNoteToElement = remember { mutableStateOf(-1) }
+    val newNoteTitle = remember { mutableStateOf("") }
+    val newNoteText = remember { mutableStateOf("") }
+
+    if (addNoteToElement.value == characterId) {
+        Column(
+            modifier = Modifier
+                .padding(4.dp)
+                .background(GrayBackground)
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            Text(text = "Add note", fontWeight = FontWeight.Bold)
+            OutlinedTextField(
+                value = newNoteTitle.value,
+                onValueChange = { newNoteTitle.value = it },
+                label = { Text(text = "Note title") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    value = newNoteText.value,
+                    onValueChange = { newNoteText.value = it },
+                    label = { Text(text = "Note content") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                )
+
+                Button(onClick = {
+                    val note = Note(characterId, newNoteTitle.value, newNoteText.value)
+                    collectionViewModel.addNote(note)
+                    newNoteTitle.value = ""
+                    newNoteText.value = ""
+                    addNoteToElement.value = -1
+                }) {
+                    Icon(Icons.Default.Check, contentDescription = null)
+                }
+            }
+        }
+    }
+
+    Button(onClick = { addNoteToElement.value = characterId }) {
+        Icon(Icons.Default.Add, contentDescription = null)
+    }
 }
